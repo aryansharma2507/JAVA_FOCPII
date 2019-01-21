33a0a40ee057b3229d13f52ed5e7447292d6b18b
aryansharma2507 <arysha2000@gmail.com>
2019-01-21 12:20:55 +0530

    initial push

--- assingnment/src/project.java
+++ assingnment/src/project.java
@@ -1,3 +1,22 @@
 class project{
-	
+	String name;
+	Date startDate;
+	Date endDate;
+	String role;
+	String [] responsibilities;
+
+	project(){
+		name = "";
+		startDate = null;
+		endDate = null;
+		role = "";
+		responsibilities = "";
+
+	}
+		project(String name, Date startDate, Date endDate, String role, String [] responsibilities){
+			this.name = name;
+			this.startDate = startDate;
+			this.role = role;
+			this.responsibilities = responsibilities;
+		}
 }
